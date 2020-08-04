0e377e58e74becf43e3f4361725f0db2ecde8d3c
João Pedro Toledo <31158250+JoaoPedroToledo@users.noreply.github.com>
2020-08-04 19:25:24 -0300

    Add files via upload

---
+++ src/components/Footer/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="https://www.alura.com.br/">
+        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      </a>
+      <p>
+        Orgulhosamente criado durante a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          Imersão React da Alura
+        </a>
+      </p>
+      <p>
+        Desenvolvido por
+        {' '}
+        <a href="https://github.com/JoaoPedroToledo">
+          João Pedro Toledo
+        </a>
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
